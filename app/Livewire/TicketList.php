bd2b291b4c2f8a7d980c65c3a06f0e046c900ec1
rudzzz <rudsolza13@gmail.com>
2025-05-08 10:49:55 -0300

    feat: add search filters for sattus and priority in TicketList

--- app/Livewire/TicketList.php
+++ app/Livewire/TicketList.php
@@ -4,12 +4,38 @@ namespace App\Livewire;
 
 use App\Models\Ticket;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TicketList extends Component
 {
+    use WithPagination;
+
+    public $status = '';
+    public $priority = '';
+
+    public function updatedStatus()
+    {
+        $this->resetPage();
+    }
+
+    public function updatedPriority()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        $tickets = Ticket::latest()->paginate(10);
+        $ticketsQuery = Ticket::query();
+
+        if ($this->status) {
+            $ticketsQuery->where('status', $this->status);
+        }
+
+        if ($this->priority) {
+            $ticketsQuery->where('priority', $this->priority);
+        }
+
+        $tickets = $ticketsQuery->latest()->paginate(10);
 
         return view('livewire.ticket-list', compact('tickets'));
     }
--- resources/views/livewire/ticket-list.blade.php
+++ resources/views/livewire/ticket-list.blade.php
@@ -1,6 +1,28 @@
 <div class="space-y-4">
     <h1 class="text-3xl font-bold py-4">Tickets</h1>
 
+    <div>
+        <label for="status" class="block text-sm font-medium">Status</label>
+        <select wire:model.lazy="status" id="status"
+            class="mt-1 block w-full border-gray-300 rounded-md text-primaryDark">
+            <option value="">All</option>
+            <option value="open">Open</option>
+            <option value="in_progress">In Progress</option>
+            <option value="closed">Closed</option>
+        </select>
+    </div>
+
+    <div>
+        <label for="priority" class="block text-sm font-medium">Priority</label>
+        <select wire:model.lazy="priority" id="priority"
+            class="mt-1 block w-full border-gray-300 rounded-md text-primaryDark">
+            <option value="">All</option>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+        </select>
+    </div>
+
     @foreach ($tickets as $ticket)
         <div class="p-4 bg-white shadow rounded text-primaryDark">
             <h2 class="text-lg font-semibold">{{ $ticket->title }}</h2>
